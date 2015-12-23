503c1daaad0aae681cb4f7c27ee80d36b493eaa7
edmundoferreira <fc.edmundo@gmail.com>
2015-12-23 08:54:28 +0100

    - updated aerostructures with aircraft frame of reference - python script with these rotations

---
+++ exercises/rotations.py
@@ -0,0 +1,15 @@
+import sympy
+
+
+theta = sympy.Symbol('theta')
+alfa  = sympy.Symbol('alfa')
+
+#A, B, C, D, E, F, G, K = [sympy.MatrixSymbol(a, n, n) for a in 'ABCDEFGK']
+
+
+#X = sympy.BlockMatrix([[A, B], [C, D]])
+#
+#Y = sympy.BlockMatrix([[E, F], [G, K]])
+
+#print latex(X*Y)
+print theta
